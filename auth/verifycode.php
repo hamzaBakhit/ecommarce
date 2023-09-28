e5ab667a6f6ce951ad9da35a123794f050538563
hamzaBakhit <hamzaqwer0599@gmail.com>
2023-09-28 13:31:54 +0300

    problem soling erify code

--- auth/verifycode.php
+++ auth/verifycode.php
@@ -5,7 +5,7 @@ include "../connect.php";
 $email = filterRequest("email");
 $verify = filterRequest("verifycode");
 
-$stmt = $con->prepare("SELECT * FROM users WHERE users_email = '$email' AND user_verifycode = '$verify' ");
+$stmt = $con->prepare("SELECT * FROM users WHERE users_email = '$email' AND users_verifycode = '$verify' ");
 
 $stmt->execute();
 
